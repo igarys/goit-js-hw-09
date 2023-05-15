418563565a73d0cd3a655e637be4e5e9ec7183c8
Iga Ryś <imirys96@gmail.com>
2023-05-16 00:46:23 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,76 +20,75 @@ timerEl.style.justifyContent = 'space-evenly';
 timerEl.style.marginTop = "150px";
 timerEl.style.fontSize = "50px";
 
-let selectedDatesTime = selectedDates[0].getTime();
-let realTime = new Date().getTime();
-let calculateMS = selectedDatesTime - realTime;
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-
+    
     onClose(selectedDates) {
-        
         if (new Date() > selectedDates[0]) {
             return (
                 (startBtn.disabled = true),
                 Notiflix.Notify.warning('Please choose a date in the future')
-            );
-        } else {
-            startBtn.disabled = false;
-            startBtn.removeEventListener('click', startTimer);
-            clearInterval(timer);
-        };
-    },
-};
-
-function startTimer() {
-    clearInterval(timer);
-    startBtn.disabled = true;
-    dateEl.disabled = true;
-    const addLeadingZero = value => value.toString().padStart(2, '0');
-            
-
-            timer = setInterval(() => {
-                const second = 1000;
-                const minute = second * 60;
-                const hour = minute * 60;
-                const day = hour * 24;
-
-                const days = Math.floor(calculateMS / day);
-                const hours = Math.floor((calculateMS % day) / hour);
-                const minutes = Math.floor(((calculateMS % day) % hour) / minute);
-                const seconds = Math.floor(
-                    (((calculateMS % day) % hour) % minute) / second
                 );
-                calculateMS -= 1000;
+            } else {
+                return (
+                    (startBtn.disabled = false),
+                    // startBtn.removeEventListener('click', startTimer),
+                    clearInterval(timer)
+                    );
+                }
+            },
+        };
+       const fp = flatpickr(dateEl, options);
+        
+       function startTimer() {
+           fp;
+           let selectedDatesTime = fp.selectedDates[0].getTime();
+           let realTime = new Date().getTime();
+           let calculateMS = selectedDatesTime - realTime;
+    //   console.log(selectedDates[0]);
 
-                daysTimer.textContent = addLeadingZero(days);
-                hoursTimer.textContent = addLeadingZero(hours);
-                minutesTimer.textContent = addLeadingZero(minutes);
-                secondsTimer.textContent = addLeadingZero(seconds);
-           
-    
-            if (calculateMS < 1) {
-                
-                clearInterval(timer);
-                dateEl.disabled = false;
-                Notiflix.Notify.success("TIMER'S UP!")
-                startBtn.removeEventListener('click', startTimer);
-            }
-        }, 1000)
-        
-        
-    };
-    console.log(selectedDates[0]);
+      clearInterval(timer);
+      startBtn.disabled = true;
+      dateEl.disabled = true;
+      const addLeadingZero = value => value.toString().padStart(2, '0');
+
+      timer = setInterval(() => {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        const days = Math.floor(calculateMS / day);
+        const hours = Math.floor((calculateMS % day) / hour);
+        const minutes = Math.floor(((calculateMS % day) % hour) / minute);
+        const seconds = Math.floor(
+          (((calculateMS % day) % hour) % minute) / second
+        );
+        calculateMS -= 1000;
+
+        daysTimer.textContent = addLeadingZero(days);
+        hoursTimer.textContent = addLeadingZero(hours);
+        minutesTimer.textContent = addLeadingZero(minutes);
+        secondsTimer.textContent = addLeadingZero(seconds);
+
+        if (calculateMS < 1) {
+          clearInterval(timer);
+          dateEl.disabled = false;
+          Notiflix.Notify.success("TIMER'S UP!");
+        //   startBtn.removeEventListener('click', startTimer);
+        }
+      }, 1000);
+    }
+
     console.log(new Date());
-    startBtn.addEventListener("click", startTimer)
-flatpickr(dateEl, options);
-
-
-
+startBtn.addEventListener("click", startTimer);
+    
+    
+    
 
 
 
